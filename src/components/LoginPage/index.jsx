48c5fa85e683b909a727a31a9d042e0d14c9a0e4
harshgeek-wq <harsh.kkr250706@gmail.com>
2026-06-30 22:15:40 +0530

    add css to loginpage

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -3,18 +3,45 @@ import "./index.css"
 
 const LoginPage = () => {
   return (
-   
-   <div className='login-box'>
-    <form action="" className='login-form'>
-        <h1>TRENDZ<span>PRIME</span></h1>
-        <p>Enter credentials to securely connect to the store</p>
-         <label htmlFor="username">USERNAME</label>
-         <input type="text" name="" id="username" placeholder='Enter your name...' />
-         <label htmlFor="password">PASSWORD</label>
-         <input type="password" name="" id="password" placeholder='Enter your Password...' />
-         <button type="button">Sign In</button>
-    </form>
-   </div>
+    <div className='login-box'>
+      <form action="" className='login-form'>
+
+        <h1 className='logo'>
+          TRENDZ<span>PRIME</span>
+        </h1>
+
+        <p className='login-text'>
+          Enter credentials to securely connect to inventory pipelines
+        </p>
+
+        <label className='login-label' htmlFor="username">
+          USERNAME
+        </label>
+
+        <input
+          className='login-input'
+          type="text"
+          id="username"
+          placeholder='Enter your name...'
+        />
+
+        <label className='login-label' htmlFor="password">
+          PASSWORD
+        </label>
+
+        <input
+          className='login-input'
+          type="password"
+          id="password"
+          placeholder='Enter your Password...'
+        />
+
+        <button className='login-btn' type="button">
+          Sign In
+        </button>
+
+      </form>
+    </div>
   )
 }
 
